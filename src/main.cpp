e49ed663aa11104e1adf9da4de6e61b786d10af7
vladislavyevsikov <vladislav.yevsikov@beetweb.com>
2026-08-13 23:26:36 +0300

    Add variables

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,14 @@
 #include <Arduino.h>
 
-#define POT_PIN 5
-#define MIN_TONE_HZ 200
-#define MAX_TONE_HZ 2000
+#define POT_PIN 4
+#define SERVO_PIN 5
 
 
-
-
-void setup() {
-  Serial.begin(115200);
-  pinMode(POT_PIN, INPUT);
-  analogSetPinAttenuation(POT_PIN, ADC_11db);
-
-}
-
-void loop() {
-  int potValue = analogRead(POT_PIN);
-  int buzzerFrequency = map(potValue, 0, 4095, MIN_TONE_HZ, MAX_TONE_HZ);
-  Serial.print("Potentiometer Value: ");
-  Serial.print(potValue);
-  delay(100);
-}
-
+constexpr int POT_RAW_MIN = 0;
+constexpr int POT_RAW_MAX = 4095;
+constexpr int SERVO_MIN_ANGLE = 0;
+constexpr int SERVO_MAX_ANGLE = 180;
+constexpr uint16_t SERVO_MIN_US = 500;
+constexpr uint16_t SERVO_MAX_US = 2500;
+constexpr uint32_t SERVO_FREQUENCY_HZ = 50;
+constexpr uint8_t SERVO_RESOLUTION_BITS = 14;
